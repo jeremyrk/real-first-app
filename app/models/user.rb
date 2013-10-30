fa6ed215bf5d8ae2d52c8bf9fc4aa1dff9b980b7
Jeremy K <jeremy.rkw@gmail.com>
2013-10-30 00:28:03 -0700

    Associated users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+  has_many :posts
 end
