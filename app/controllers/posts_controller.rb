d89f5f90b8380783dc6130fa0042d1baa53aaff0
Jeremy K <jeremy.rkw@gmail.com>
2013-10-31 21:33:29 -0700

    Associated and nested topics and posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,49 +1,56 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+  # def index
+  #   @posts = Post.all
+  # end
 
   def show
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
   end
 
   def new
     @post = Post.new
+    @topic = Topic.find(params[:topic_id])
     authorize! :create, Post, message: "You need to be a member to create a new post."
-  end
-
-  def create
-    @post = current_user.posts.build(params[:post])
-
-    authorize! :create, @post, message: "You need to be signed up to do that."
-    if @post.save
-    #@post = Post.new(params[:post])
-    #raise
-      if @post.save
-       flash[:notice] = "Post was saved."
-       redirect_to @post
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :new
-      end
   end
 
   def edit
     @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
     authorize! :edit, @post, message: "You need to own the post to edit it."
   end
 
-  def update
-    @post = Post.find(params[:id])
-    if @post.update_attributes(params[:post])
-      flash[:notice] = "Post was updated."
-      redirect_to @post
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
+  def create
+    @post = current_user.posts.build(params[:post])
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
 
-end 
+    authorize! :create, @post, message: "You need to be signed up to do that."
+ #   if @post.save
+    #@post = Post.new(params[:post])
+    #raise
+      if @post.save
+       flash[:notice] = "Post was saved."
+       redirect_to [@topic, @post]
+      else
+        flash[:error] = "There was an error saving the post. Please try again."
+        render :new
+      end
+#  end
+
+    def update
+      @post = Post.find(params[:id])
+      @topic = Topic.find(params[:topic_id])
+
+      authorize! :update, @post, message: "You need to own the post to edit it."
+      if @post.update_attributes(params[:post])
+        flash[:notice] = "Post was updated."
+        redirect_to @post
+      else
+        flash[:error] = "There was an error saving the post. Please try again."
+        render :edit
+      end
+    end
+  end 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@
 
 Bloccit::Application.routes.draw do
 
+  # get "topics/index"
+  # get "topics/new"
+  # get "topics/show"
+  # get "topics/edit"
   devise_for :users
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   match "about" => 'welcome#about', via: :get
 
@@ -16,6 +22,10 @@ end
 #####    OLD
 
 # Bloccit::Application.routes.draw do
+  # get "topics/index"
+  # get "topics/new"
+  # get "topics/show"
+  # get "topics/edit"
 #  devise_for :users
 #   get "posts/index"
 #   get "posts/show"
